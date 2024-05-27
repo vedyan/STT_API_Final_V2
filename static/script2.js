577130c4aafaba53e8ba8667f5853c2c990f82f5
vedyan <156664713+vedyan@users.noreply.github.com>
2024-05-28 01:08:37 +0530

    Add files via upload

---
+++ static/script2.js
@@ -0,0 +1,23 @@
+var socket = io.connect(
+    "http://" + window.location.hostname + ":" + location.port
+  );
+  
+  var isTranscribing = false;
+  
+  document.getElementById("startButton").addEventListener("click", function () {
+    if (!isTranscribing) {
+      // Start transcription
+      isTranscribing = true;
+      socket.emit("toggle_transcription", { action: "start" });
+      document.getElementById("startButton").innerText = "Stop";
+    } else {
+      // Stop transcription
+      isTranscribing = false;
+      socket.emit("toggle_transcription", { action: "stop" });
+      document.getElementById("startButton").innerText = "Start";
+    }
+  });
+  
+  socket.on("transcription_update", function (data) {
+    document.getElementById("captions").innerHTML = data.transcription;
+  });  
